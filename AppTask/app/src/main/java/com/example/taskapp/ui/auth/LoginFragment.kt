ef00dfb6c4d409f829c4eb54b00874ec72ca1b10
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-08 21:22:45 -0300

    Add funcionalidade login no app

--- AppTask/app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
+++ AppTask/app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentLoginBinding
 import com.example.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,11 +34,13 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         initListeners()
     }
 
-    private fun initListeners(){
-        binding.btnRegister.setOnClickListener{
+    private fun initListeners() {
+        binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
@@ -39,23 +48,35 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
         }
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             validateData()
         }
     }
 
-    private fun validateData(){
+    private fun validateData() {
         val email = binding.editEmail.text.toString().trim()
         val password = binding.editPassword.text.toString().trim()
 
-        if(email.isNotEmpty()){
-            if(password.isNotEmpty()){
-                findNavController().navigate(R.id.action_global_homeFragment)
-            }else{
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                binding.progressBar.isVisible = true
+                loginUser(email, password)
+            } else {
                 showBottomSheet(message = getString(R.string.password_empty_warning_login))
             }
-        }else{
+        } else {
             showBottomSheet(message = getString(R.string.email_empty_warning_login))
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+            if (task.isSuccessful){
+                findNavController().navigate(R.id.action_global_homeFragment)
+            }else{
+                binding.progressBar.isVisible = false
+                Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
