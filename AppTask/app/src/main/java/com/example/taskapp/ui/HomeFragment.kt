e6172c8b792e3108f2bf29f1416a5f0cb2dbf13b
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-08 21:13:02 -0300

    Add funcionalidade de deslogar usuario

--- AppTask/app/src/main/java/com/example/taskapp/ui/HomeFragment.kt
+++ AppTask/app/src/main/java/com/example/taskapp/ui/HomeFragment.kt
@@ -5,27 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentHomeBinding
 import com.example.taskapp.ui.adapter.ViewPagerAdapter
+import com.example.taskapp.util.showBottomSheet
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = com.example.taskapp.databinding.FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
+        initListeners()
         initTabs()
     }
 
@@ -43,6 +53,20 @@ class HomeFragment : Fragment() {
             tab.text = getString(pagerAdapter.getTitle(position))
         }.attach()
 
+    }
+
+    private fun initListeners(){
+        binding.btnLogout.setOnClickListener{
+            showBottomSheet(
+                titleDialog = R.string.text_title_dialog_confirm_logout,
+                titleButton = R.string.text_button_dialog_confirm_logout,
+                message = getString(R.string.text_message_dialog_confirm_logout),
+                onClick ={
+                    auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_authentication)
+                }
+            )
+        }
     }
 
     override fun onDestroyView() {
