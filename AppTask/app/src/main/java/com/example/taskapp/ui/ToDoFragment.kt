4f352ed07f9f3f8420c3ae757401a20809c9ae16
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-09 15:06:48 -0300

    Criando loading e filtrando tasks por status

--- AppTask/app/src/main/java/com/example/taskapp/ui/ToDoFragment.kt
+++ AppTask/app/src/main/java/com/example/taskapp/ui/ToDoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -104,12 +105,19 @@ class ToDoFragment : Fragment() {
             .child("tasks")
             .child(auth.currentUser?.uid ?: "")
             .addValueEventListener(object : ValueEventListener {
+
                 override fun onDataChange(snapshot: DataSnapshot) {
                     val taskList = mutableListOf<Task>()
+
                     for (ds in snapshot.children) {
                         val task = ds.getValue(Task::class.java) as Task
-                        taskList.add(task)
+                        if (task.status == Status.TODO) {
+                            taskList.add(task)
+                        }
                     }
+
+                    binding.progressBar.isVisible = false
+                    listEmpty(taskList)
                     taskAdapter.submitList(taskList)
                 }
 
@@ -118,6 +126,14 @@ class ToDoFragment : Fragment() {
                         .show()
                 }
             })
+    }
+
+    private fun listEmpty(taskList: List<Task>) {
+        binding.textInfo.text = if (taskList.isEmpty()) {
+            getString(R.string.text_list_task_empty)
+        } else {
+            ""
+        }
     }
 
     override fun onDestroyView() {
