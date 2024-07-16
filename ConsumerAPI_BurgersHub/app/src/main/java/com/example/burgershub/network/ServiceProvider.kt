5e1eb94cb32d6357e36c380c39519d2ca4c5684d
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-15 23:45:37 -0300

    Listando Hamburgers

--- ConsumerAPI_BurgersHub/app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ ConsumerAPI_BurgersHub/app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,6 +1,8 @@
 package com.example.burgershub.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -15,6 +17,21 @@ class ServiceProvider {
     private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor(Interceptor { chain ->
+            chain.run {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            "x-rapidapi-key",
+                            "c5a5f6e406mshe9784b4c1fb7fa1p1e5cd7jsn63a0059ab09e")
+                        .build()
+                )
+            }
+        })
         .build()
 
     private val retrofit = Retrofit.Builder()
