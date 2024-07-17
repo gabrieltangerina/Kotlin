8fd09d88748ec037050448d756347c0a384cd227
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-17 10:48:12 -0300

    Adicionando Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    // Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.0-beta02")
+
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-auth")
