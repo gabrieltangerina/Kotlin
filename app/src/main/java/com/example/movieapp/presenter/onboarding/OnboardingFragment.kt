c4df19211f1017449a32ba828a5a3aa944e49d6e
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-17 11:02:07 -0300

    Criando tela de onboarding

---
+++ app/src/main/java/com/example/movieapp/presenter/onboarding/OnboardingFragment.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.presenter.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+
+class OnboardingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+
+}
