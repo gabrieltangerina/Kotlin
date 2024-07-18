55e9df8ea358ad8631922e7f5799d7eb1f5d9397
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-17 22:32:53 -0300

    Criando tela de recuperacao da conta

---
+++ app/src/main/java/com/example/movieapp/presenter/auth/forgot/ForgotPasswordFragment.kt
@@ -0,0 +1,20 @@
+package com.example.movieapp.presenter.auth.forgot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+}
