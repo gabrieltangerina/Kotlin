897dd703b995fbf17942d0f289e85e3c61446b24
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-18 21:02:59 -0300

    Add funcionalidade registrar usuario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
@@ -44,6 +47,17 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Lifecycle (usar o emit no viewModel)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
 
     // Splash Screen
     implementation("androidx.core:core-splashscreen:1.0.0-beta02")
---
+++ app/src/main/java/com/example/movieapp/presenter/auth/register/RegisterViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.movieapp.presenter.auth.register
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.movieapp.domain.usecase.auth.RegisterUseCase
+import com.example.movieapp.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val registerUseCase: RegisterUseCase
+) : ViewModel() {
+
+    fun register(email: String, password: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            registerUseCase.invoke(email, password)
+
+            emit(StateView.Success(Unit))
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            emit(StateView.Error(ex.message))
+        }
+    }
+
+}
