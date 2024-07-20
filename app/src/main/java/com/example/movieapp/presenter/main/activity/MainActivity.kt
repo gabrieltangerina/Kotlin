2704a687c3e7c5a6a0b623206e322f2cb9fb551c
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-20 16:20:33 -0300

    Configurando BottomBar

---
+++ app/src/main/java/com/example/movieapp/presenter/main/activity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.movieapp.presenter.main.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.movieapp.R
+import com.example.movieapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.btnv, navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, args ->
+            binding.btnv.isVisible =
+                destination.id == R.id.menu_home ||
+                        destination.id == R.id.menu_search ||
+                        destination.id == R.id.menu_favorite ||
+                        destination.id == R.id.menu_download ||
+                        destination.id == R.id.menu_profile
+        }
+    }
+}
