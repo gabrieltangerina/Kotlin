1ac2028eab1a8af283d2f37e25217d6509515b9e
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-21 02:16:12 -0300

    Configurando Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,17 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    // Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    // OKHttp
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
