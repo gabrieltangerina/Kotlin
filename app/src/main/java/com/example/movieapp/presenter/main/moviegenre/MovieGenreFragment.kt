ff2884cd5236b51d138c38332051697eb664630a
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-23 10:30:58 -0300

    Listando filmes na tela de listagem por genero

--- app/src/main/java/com/example/movieapp/presenter/main/moviegenre/MovieGenreFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/main/moviegenre/MovieGenreFragment.kt
@@ -6,18 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentHomeBinding
 import com.example.movieapp.databinding.FragmentMovieGenreBinding
+import com.example.movieapp.presenter.main.bottombar.home.HomeFragmentDirections
+import com.example.movieapp.presenter.main.bottombar.home.adapter.GenreMovieAdapter
+import com.example.movieapp.presenter.main.bottombar.home.adapter.MovieAdapter
+import com.example.movieapp.presenter.model.GenrePresentation
+import com.example.movieapp.util.StateView
 import com.example.movieapp.util.initToolbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MovieGenreFragment : Fragment() {
 
     private var _binding: FragmentMovieGenreBinding? = null
     private val binding get() = _binding!!
 
-    private val args : MovieGenreFragmentArgs by navArgs()
+    private val args: MovieGenreFragmentArgs by navArgs()
+
+    private lateinit var movieAdapter: MovieAdapter
+    private val viewModel: MovieGenreViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +49,37 @@ class MovieGenreFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initToolbar(binding.toolbar)
+        initRecycler()
+        getMoviesByGenre()
+    }
+
+    private fun initRecycler() {
+        movieAdapter = MovieAdapter(requireContext())
+
+        with(binding.recyclerMovies) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            setHasFixedSize(true)
+            adapter = movieAdapter
+        }
+    }
+
+    private fun getMoviesByGenre() {
+        viewModel.getMoviesByGenre(args.genreId).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+
+                is StateView.Success -> {
+                    movieAdapter.submitList(stateView.data)
+                    binding.progressBar.isVisible = false
+                }
+
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/movieapp/presenter/main/moviegenre/MovieGenreViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.movieapp.presenter.main.moviegenre
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.movieapp.BuildConfig
+import com.example.movieapp.domain.usecase.movie.GetMoviesByGenreUseCase
+import com.example.movieapp.util.Constants
+import com.example.movieapp.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieGenreViewModel @Inject constructor(
+    private val getMoviesByGenreUseCase: GetMoviesByGenreUseCase
+) : ViewModel() {
+
+    fun getMoviesByGenre(genreId: Int?) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            val movies = getMoviesByGenreUseCase.invoke(
+                BuildConfig.API_KEY,
+                Constants.Movie.LANGUAGE,
+                genreId = genreId
+            )
+
+            emit(StateView.Success(movies))
+
+        }catch (ex: HttpException){
+            ex.printStackTrace()
+            emit(StateView.Error(ex.message))
+        }catch (ex: Exception){
+            ex.printStackTrace()
+            emit(StateView.Error(ex.message))
+        }
+    }
+
+}
