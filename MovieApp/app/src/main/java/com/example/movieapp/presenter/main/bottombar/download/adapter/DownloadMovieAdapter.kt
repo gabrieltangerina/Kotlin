8a947ffba8dec7299802f5fed6932ed91383530c
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-07-29 20:26:23 -0300

    Configurando adapter de listagem dos filmes baixados

---
+++ MovieApp/app/src/main/java/com/example/movieapp/presenter/main/bottombar/download/adapter/DownloadMovieAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.movieapp.presenter.main.bottombar.download.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movieapp.R
+import com.example.movieapp.databinding.MovieDownloadItemBinding
+import com.example.movieapp.domain.model.Movie
+
+class DownloadMovieAdapter(
+    private val context: Context,
+    private val detailsClickListener: (Int?) -> Unit,
+    private val deleteClickListener: (Int?) -> Unit
+) : ListAdapter<Movie, DownloadMovieAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Movie>() {
+
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            MovieDownloadItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val movie = getItem(position)
+
+        Glide
+            .with(context)
+            .load("https://image.tmdb.org/t/p/w200${movie.posterPath}")
+            .into(holder.binding.ivMovie)
+
+        holder.binding.textMovie.text = movie.title
+        holder.binding.textDuration.text = movie.runtime.toString()
+        holder.binding.textSize.text = movie.runtime.toString()
+        holder.binding.ibDelete.setOnClickListener { deleteClickListener(movie.id) }
+        holder.itemView.setOnClickListener { detailsClickListener(movie.id) }
+    }
+
+    inner class MyViewHolder(val binding: MovieDownloadItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val movieImage: ImageView
+
+        init {
+            movieImage = itemView.findViewById(R.id.movie_image)
+        }
+    }
+
+}
