d187b7e5fe253155848576d3851dcbd24e1a3fa2
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-08 20:11:30 -0300

    Validando informacoes na tela de editar perfil

--- MovieApp/app/src/main/java/com/example/movieapp/presenter/main/bottombar/profile/edit/EditProfileFragment.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/presenter/main/bottombar/profile/edit/EditProfileFragment.kt
@@ -9,6 +9,8 @@ import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentEditProfileBinding
 import com.example.movieapp.databinding.FragmentProfileBinding
 import com.example.movieapp.util.initToolbar
+import com.example.movieapp.util.isEmailValid
+import com.example.movieapp.util.showSnackBar
 
 
 class EditProfileFragment : Fragment() {
@@ -28,6 +30,34 @@ class EditProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initToolbar(binding.toolbar)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.btnUpdate.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun validateData(){
+        val name = binding.editName.text.toString().trim()
+        val lastName = binding.editUser.text.toString().trim()
+        val phone = binding.editPhone.text.toString().trim()
+        val genre = binding.editGenre.text.toString().trim()
+        val country = binding.editCountry.text.toString().trim()
+
+        if(name.isEmpty()){
+            showSnackBar(message = R.string.text_name_empty_edit_profile_fragment)
+        }else if(lastName.isEmpty()){
+            showSnackBar(message = R.string.text_user_empty_edit_profile_fragment)
+        }else if(phone.isEmpty()){
+            showSnackBar(message = R.string.text_phone_empty_edit_profile_fragment)
+        }else if(genre.isEmpty()){
+            showSnackBar(message = R.string.text_genre_empty_edit_profile_fragment)
+        }else if(country.isEmpty()){
+            showSnackBar(message = R.string.text_country_empty_edit_profile_fragment)
+        }
+
     }
 
     override fun onDestroyView() {
