62cbf14f645696e8a89ff98372b67b85de541cbc
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-10 20:47:41 -0300

    Criando layout da tela de recuperar conta

---
+++ BancoDigital/app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverFragment.kt
@@ -0,0 +1,30 @@
+package com.example.bancodigital.presenter.auth.recover
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bancodigital.R
+import com.example.bancodigital.databinding.FragmentRecoverBinding
+import com.example.bancodigital.databinding.FragmentSplashBinding
+
+class RecoverFragment : Fragment() {
+
+    private var _binding: FragmentRecoverBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoverBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
