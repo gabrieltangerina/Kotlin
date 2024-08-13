3015a160213da3a8705e3393b8fc1447474ad6ed
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-13 20:47:45 -0300

    Configurando metodo para login

--- BancoDigital/app/src/main/java/com/example/bancodigital/data/repository/auth/AuthFirebaseDataSourceImpl.kt
+++ BancoDigital/app/src/main/java/com/example/bancodigital/data/repository/auth/AuthFirebaseDataSourceImpl.kt
@@ -2,29 +2,43 @@ package com.example.bancodigital.data.repository.auth
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
 import kotlin.coroutines.suspendCoroutine
 
 class AuthFirebaseDataSourceImpl(
-    private val firebaseDatabase: FirebaseDatabase,
     private val firebaseAuth: FirebaseAuth
 ) : AuthFirebaseDataSource {
 
     override suspend fun login(email: String, password: String) {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        continuation.resumeWith(Result.success(Unit))
+                    } else {
+                        task.exception?.let {
+                            continuation.resumeWith(Result.failure(it))
+                        }
+                    }
+                }
+        }
     }
 
-    override suspend fun register(name: String, email: String, phone: String, password: String): FirebaseUser {
-        return suspendCoroutine {continuation ->
+    override suspend fun register(
+        name: String,
+        email: String,
+        phone: String,
+        password: String
+    ): FirebaseUser {
+        return suspendCoroutine { continuation ->
             firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener{task ->
-                    if(task.isSuccessful){
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
                         val user = task.result.user
 
                         user?.let {
                             continuation.resumeWith(Result.success(it))
                         }
-                    }else{
+                    } else {
                         task.exception?.let {
                             continuation.resumeWith(Result.failure(it))
                         }
