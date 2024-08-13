2f51515bc5f7ffd2950a15bf9a8de4e0b542fd8a
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-12 21:41:25 -0300

    Configurando instancia do Firebase

--- BancoDigital/app/src/main/java/com/example/bancodigital/MyApplication.kt
+++ BancoDigital/app/src/main/java/com/example/bancodigital/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.example.bancodigital
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -8,6 +9,7 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
     }
 
 }
