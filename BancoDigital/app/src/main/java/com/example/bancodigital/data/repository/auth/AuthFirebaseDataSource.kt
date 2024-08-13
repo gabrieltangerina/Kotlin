c026282e66111e02328d4f353423ab11918c62ea
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-12 21:13:18 -0300

    Criando camada de Data

---
+++ BancoDigital/app/src/main/java/com/example/bancodigital/data/repository/auth/AuthFirebaseDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.bancodigital.data.repository.auth
+
+interface AuthFirebaseDataSource {
+
+    suspend fun login(
+        email: String,
+        password: String
+    )
+
+    suspend fun register(
+        name: String,
+        email: String,
+        phone: String,
+        password: String
+    )
+
+    suspend fun recover(
+        email: String
+    )
+
+}
---
+++ BancoDigital/app/src/main/java/com/example/bancodigital/data/repository/auth/AuthFirebaseDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.bancodigital.data.repository.auth
+
+import com.google.firebase.database.FirebaseDatabase
+
+class AuthFirebaseDataSourceImpl(
+    private val firebaseDatabase: FirebaseDatabase
+) : AuthFirebaseDataSource {
+
+    override suspend fun login(email: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun register(name: String, email: String, phone: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun recover(email: String) {
+        TODO("Not yet implemented")
+    }
+
+}
