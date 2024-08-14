a9bfb5159b4165b372a76be54a811b33ab37e7c0
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-14 11:07:22 -0300

    Criando navegacao do app e ajustes de ui

--- BancoDigital/app/build.gradle.kts
+++ BancoDigital/app/build.gradle.kts
@@ -57,27 +57,12 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
 
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
-
-//dependencies {
-//    implementation("androidx.core:core-ktx:1.13.1")
-//    implementation("androidx.appcompat:appcompat:1.7.0")
-//    implementation("com.google.android.material:material:1.12.0")
-//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-//
-//    // Firebase
-//    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-//    implementation("com.google.firebase:firebase-auth")
-//    implementation("com.google.firebase:firebase-database")
-//
-//    // Dagger Hilt
-//    implementation("com.google.dagger:hilt-android:2.51.1")
-//    kapt("com.google.dagger:hilt-compiler:2.51.1")
-//}
