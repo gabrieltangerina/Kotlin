c44b37cce8cfceec13eb836d64f6dbd4de72fa8e
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-15 10:30:54 -0300

    Criando LoginViewModel e RecoverViewModel

---
+++ BancoDigital/app/src/main/java/com/example/bancodigital/presenter/auth/login/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.bancodigital.presenter.auth.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.bancodigital.domain.auth.LoginUseCase
+import com.example.bancodigital.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : ViewModel() {
+
+    fun login(email: String, password: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            loginUseCase.invoke(email, password)
+
+            emit(StateView.Sucess(Unit))
+
+        }catch (ex: Exception){
+            emit(StateView.Error(ex.message))
+        }
+    }
+
+}
---
+++ BancoDigital/app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.bancodigital.presenter.auth.recover
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.bancodigital.domain.auth.RecoverUseCase
+import com.example.bancodigital.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class RecoverViewModel @Inject constructor(
+    private val recoverUseCase: RecoverUseCase
+) : ViewModel() {
+
+    fun recover(email: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            recoverUseCase.invoke(email)
+
+            emit(StateView.Sucess(Unit))
+
+        }catch (ex: Exception){
+            emit(StateView.Error(ex.message))
+        }
+    }
+
+}
