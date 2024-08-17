ae2468d637dcd89fef1c65cbc345fbfdd1091423
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-17 15:17:40 -0300

    Criando funcionalidade de recuperacao de conta

--- BancoDigital/app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverFragment.kt
+++ BancoDigital/app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.bancodigital.R
 import com.example.bancodigital.databinding.FragmentRecoverBinding
+import com.example.bancodigital.util.StateView
 import com.example.bancodigital.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +20,8 @@ class RecoverFragment : Fragment() {
 
     private var _binding: FragmentRecoverBinding? = null
     private val binding get() = _binding!!
+
+    private val recoverViewModel : RecoverViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +45,31 @@ class RecoverFragment : Fragment() {
     private fun validateData(){
         val email = binding.editEmail.text.toString().trim()
 
-        if(email.isNotEmpty()){
-
-        }else{
+        if(email.isEmpty()){
             Toast.makeText(requireContext(), "Digite um e-mail", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        recoverAccount(email)
+    }
+
+    private fun recoverAccount(email: String) {
+        recoverViewModel.recover(email).observe(viewLifecycleOwner){stateView ->
+            when(stateView){
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+
+                is StateView.Sucess -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), "Mensagem de recuperação enviada ao email", Toast.LENGTH_SHORT).show()
+                }
+
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
