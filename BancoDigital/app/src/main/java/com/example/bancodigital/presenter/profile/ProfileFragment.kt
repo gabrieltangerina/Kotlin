ec663fb2f5ef9a47ad097a56b5cc61e7427ede66
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-08-26 21:27:11 -0300

    Criando validador para salvar a alteracao no perfil do usuario

--- BancoDigital/app/src/main/java/com/example/bancodigital/presenter/profile/ProfileFragment.kt
+++ BancoDigital/app/src/main/java/com/example/bancodigital/presenter/profile/ProfileFragment.kt
@@ -3,12 +3,15 @@ package com.example.bancodigital.presenter.profile
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.bancodigital.R
 import com.example.bancodigital.data.model.User
 import com.example.bancodigital.databinding.FragmentProfileBinding
 import com.example.bancodigital.util.StateView
@@ -28,6 +31,8 @@ class ProfileFragment : Fragment() {
     private lateinit var initialPhone: String
     private var flagChangedName: Boolean = false
     private var flagChangedPhone: Boolean = false
+    private var nameValid: Boolean = false
+    private var phoneValid: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,12 +47,11 @@ class ProfileFragment : Fragment() {
 
         initToolbar(binding.toolbar)
         getProfile()
-        initObservers()
     }
 
-    private fun getProfile(){
-        profileViewModel.getProfileUseCase().observe(viewLifecycleOwner){stateView ->
-            when(stateView){
+    private fun getProfile() {
+        profileViewModel.getProfileUseCase().observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
                 is StateView.Loading -> {
                     binding.progressBar.isVisible = true
                 }
@@ -56,8 +60,6 @@ class ProfileFragment : Fragment() {
                     binding.progressBar.isVisible = false
 
                     stateView.data?.let {
-                        initialName = stateView.data.name
-                        initialPhone = stateView.data.phone
                         configData(it)
                     }
 
@@ -71,15 +73,43 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    private fun initObservers(){
-        binding.editName.addTextChangedListener(object : TextWatcher{
+    private fun configData(user: User) {
+        binding.editEmail.setText(user.email)
+        binding.editName.setText(user.name)
+        binding.editPhone.setText(user.phone)
+
+        initialName = binding.editName.text.toString()
+        initialPhone = binding.editPhone.text.toString()
+        Log.i("INFOTESTE", "initialName: $initialName | initialPhone: $initialPhone")
+        initObservers()
+    }
+
+    private fun initObservers() {
+        binding.editName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 return
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 flagChangedName = s.toString().trim() != initialName
-                binding.btnUpdate.isEnabled = flagChangedName || flagChangedPhone
+                nameValid = binding.editName.text.toString().trim() != ""
+
+                if ((flagChangedName && nameValid) || ( flagChangedPhone && phoneValid)) {
+                    binding.btnUpdate.setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.bg_btn
+                        )
+                    )
+                } else {
+                    binding.btnUpdate.setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.bg_btn_not_enabled
+                        )
+                    )
+                }
+
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -88,14 +118,31 @@ class ProfileFragment : Fragment() {
 
         })
 
-        binding.editPhone.addTextChangedListener(object : TextWatcher{
+        binding.editPhone.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 return
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 flagChangedPhone = s.toString().trim() != initialPhone
-                binding.btnUpdate.isEnabled = flagChangedName || flagChangedPhone
+                phoneValid = binding.editPhone.text.toString().length == 15
+
+                if ((flagChangedName && nameValid) || ( flagChangedPhone && phoneValid)) {
+                    binding.btnUpdate.setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.bg_btn
+                        )
+                    )
+                } else {
+                    binding.btnUpdate.setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.bg_btn_not_enabled
+                        )
+                    )
+                }
+
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -103,12 +150,6 @@ class ProfileFragment : Fragment() {
             }
 
         })
-    }
-
-    private fun configData(user: User){
-        binding.editEmail.setText(user.email)
-        binding.editName.setText(user.name)
-        binding.editPhone.setText(user.phone)
     }
 
     override fun onDestroyView() {
